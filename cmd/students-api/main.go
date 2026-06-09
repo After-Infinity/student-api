8ecd389cccbb8ce868cdbcd23b44593bbce66324
After-Infinity <aminbluestack@gmail.com>
2026-06-09 09:26:10 +0530

    done the config,set a simple route and started the server for first time

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/After-Infinity/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome To Students API")
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	fmt.Println("Server Started")
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
+
 }
